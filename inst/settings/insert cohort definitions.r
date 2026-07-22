8dc4cb9ad47a0774266c8be71682c78c9b7a7591
Ayman Hijazy <94984907+ahijazy@users.noreply.github.com>
2026-07-22 14:09:45 +0200

    Update insert cohort definitions.r

--- inst/settings/insert cohort definitions.r
+++ inst/settings/insert cohort definitions.r
@@ -2,7 +2,7 @@ library(ROhdsiWebApi)
 
 baseUrl <-"https://atlas.odysseusinc.com/WebAPI"
 
-token <- 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJheW1hbl9oaWphenlAZXBhbS5jb20iLCJTZXNzaW9uLUlEIjoiZWNiOGZlYjMtZWFmNy00MTI4LTk1NGItNzA1OWFjNGNiZjY4IiwiZXhwIjoxNzkxMzk4MzI4fQ.I7kMC04SqdSUNO1bCmZJrEJovFp09kzH6Gp_BNxg5RqOqNBw0jnD-ezYVkX19mhWcyf7PLIrQs0CeCSGg2PVMQ'
+token <- ''
 setAuthHeader(baseUrl = baseUrl, token)
 
 # after inserting the cohorts
